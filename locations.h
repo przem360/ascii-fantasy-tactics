9bf33262af07a81ff1708b60697900a123b0ba9a
przem360 <przem360@gmail.com>
2023-11-05 15:04:36 +0200

    Rewriting rooms 1, 2, 3, 4, 6, 8, 10.

--- locations.h
+++ locations.h
@@ -6,7 +6,7 @@ struct locations {
     int id; // number of room on map
     int size[2]; // width west to east, lenght north to south
     char name[32];
-    char description[210];
+    char description[285];
     char action_description[110];
     int is_enemy; // 0 - no enemy, 1 - look in enemy_array_index
     int enemy_array_index;
@@ -20,19 +20,19 @@ struct locations {
     int go_west;
 };
 
-// rewritten locations: 5, 7, 9, 11
-// 
+// rewritten locations: 1 - 11
+
 struct locations rooms[36] = {
-    {1,{4,6},"Entry hall","This large room has a row of heavy, cobweb-laden columns down each side.\nThe floor is dusty, and you can clearly see a beaten path to \n the door on the far side of the room.", "Nothing to do here.",0,0,0,0,/* a */0,/* c */102,0,2,0},
-    {2,{4,4},"Secret room","Description yet to come...",  "Also yet to come",1,0,0,0,/* a */0,/* c */1,0,0,0},
-    {3,{6,5},"Room","A row of pegs such as one might hang a cloak \n from run along the wall to your left. The floor is \n littered with scraps of gray and brown fabric.", "Nothing here",0,0,0,0,/* a */0,/* c */101,1,103,5},
-    {4,{4,3},"Storage room","This is obviously a storeroom. The walls to the left \n and right are lined with shelves containing what \n appear to be many bottles of wine. The far wall \n is lined with large barrels.", "Nothing here",0,0,0,0,/* a */0,/* c */103,0,0,0},
+    {1,{4,6},"Entry hall","You entered between gray brick walls, old and torn,\nprotruding from the ground like the teeth of an ancient beast.\n\nIn the northern wall there is a passage leading\nto the corridor, in the southern wall there is a heavy door.\n\nThere are sounds of giggling and wild growling.", "These sounds are coming from the south.",0,0,0,0,/* a */0,/* c */102,0,2,0},
+    {2,{4,4},"The bat chamber","This small chamber is empty.\nBats hang from the rotting ceiling beams.\nYou see that there are deep grooves in the walls.",  "Whatever was clawing at the walls was a big beast.",1,0,0,0,/* a */0,/* c */1,0,0,0},
+    {3,{6,5},"Old library","This is the sorcerer\'s library.\nThe shelves stretching along the walls to the ceiling\nare bending under the weight of thousands of books.", "The collection is impressive.",0,0,0,0,/* a */0,/* c */101,1,103,5},
+    {4,{4,3},"Sorcerer\'s collection","In this small room there are cabinets with dozens of small drawers.\nThere are jars with strange things and a collection of magic wands.", "These items may be cursed.",0,0,0,0,/* a */0,/* c */103,0,0,0},
     {5,{12,6},"The Creek of Nixx","This was once a sorcerer's workshop,\na branch of the Nixx River flows through here, who knows what may be swimming in these waters\nthat were once used for the practice of black magic.", "If you remain silent, you can listen to the sound of the slowly flowing river.",1,0,0,0,/* a */2,/* c */105,3,0,0},
-    {6,{4,2},"Study","This room appears to be a study. There is a desk \n on the left and several dilapidated chairs and a \n bookshelf on the right.", "Search reveals passage hidden behind the bookshelf",0,0,0,0,/* a */0,/* c */7,0,105,0},
+    {6,{4,2},"Room with paintings","There is no furniture in this room.\nThere are portraits and landscapes depicting\ncemeteries hanging on the walls.\nOpposite the entrance, in the northern wall, there is a door.", "You hear the sounds of footsteps.",0,0,0,0,/* a */0,/* c */7,0,105,0},
     {7,{4,3},"The Chapel of Solitude","Visibility is poor.\nThere are no windows in this room and the only\nsource of light is the door through which you entered.\nOn the opposite wall there is an armchair and a black mirror.", "You sense the gloomy aura of this room.",1,0,0,0,/* a */0,/* c */0,0,6,0},
-    {8,{4,4},"Giant spider","Debris, mostly bits of rock and dirt with a few \n bones, is scattered on the floor. The high ceiling \n of this room is covered in a mass of spiderweb.", "Giant Ants attacks!",0,0,0,0,/* a */0,/* c */108,101,0,0},
+    {8,{4,4},"The Chamber of fungus","Here the walls have vibrant colors, you become light, you seem to be floating. Would you like to stay here?", "You hear the beautiful sound of the cello.",0,0,0,0,/* a */0,/* c */108,101,0,0},
     {9,{6,4},"Temple of forgotten gods","Even though the vault collapsed long ago,\nthe columns that once supported it are still standing.\nUnder the western wall you can see the remains of a wooden altar.",  "This place will be haunted forever.",1,0,1,0,/* a */1,/* c */0,109,101,0},
-    {10,{3,3},"Bedroom","This room contains a bed, wardrobe, table and chair. \n On the table you see a ring of keys. \n A thick layer of dust covers everything.",  "Found gold!",0,0,1,0,/* a */0,/* c */0,108,0,0},
+    {10,{3,3},"The mosaics chamber","A small room with one window. The walls are lined with ceramic mosaics depicting old magical rites.",  "There\'s nothing else here.",0,0,1,0,/* a */0,/* c */0,108,0,0},
     {11,{4,5},"The Tower","There used to be a tower here but it collapsed a long time ago.\nThe remains of a staircase leading to nowhere stick out of the ground.", "You hear a strange noises.",1,0,0,0,/* a */0,/* c */0,0,108,0},
     {12,{4,4},"Bugbears","You see a group of hairy goblin-like humanoids as big as large men. \n They appear to be camping in this room",  "5 Bugbears attacks",1,0,0,0,/* a */0,/* c */0,112,113,109},
     {13,{4,3},"Empty room","This room appears to be empty.", "Found 500 GP",0,1,0,0,/* a */0,/* c */0,113,0,0},
